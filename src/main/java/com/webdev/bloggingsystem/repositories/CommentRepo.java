2cc3bcb2a7f05d81c8031297ebc7732267d613ab
shawn <shawnby222@gmail.com>
2025-11-13 16:06:07 -0600

    Adds test class for comment controller flow.

--- src/main/java/com/webdev/bloggingsystem/repositories/CommentRepo.java
+++ src/main/java/com/webdev/bloggingsystem/repositories/CommentRepo.java
@@ -3,12 +3,16 @@ package com.webdev.bloggingsystem.repositories;
 import com.webdev.bloggingsystem.entities.Comment;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface CommentRepo extends Repository<Comment, Integer> {
+public interface CommentRepo extends CrudRepository<Comment, Integer> {
     @Query(value = "SELECT c FROM Comment c JOIN FETCH c.author WHERE c.parentComment.id = :id")
     List<Comment> findAllByParentCommentId(@Param("id") Integer parentId);
+
+    @Query(value = "SELECT c FROM Comment c JOIN FETCH c.author WHERE c.id = :id")
+    Optional<Comment> getCommentById(Integer id);
 }
---
+++ src/test/java/com/webdev/bloggingsystem/controllers/CommentControllerTest.java
@@ -0,0 +1,131 @@
+package com.webdev.bloggingsystem.controllers;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.webdev.bloggingsystem.entities.LoginDto;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CommentControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private String testUserToken;
+
+    private String getToken(String username, String password) {
+        LoginDto loginDto = new LoginDto(username, password);
+
+        ResponseEntity<String> response = restTemplate
+                .postForEntity("/auth/login", loginDto, String.class);
+
+        return response.getBody();
+    }
+
+    @BeforeAll
+    public void beforeAll() {
+        this.testUserToken = this.getToken("TestUser", "TestPassword");
+    }
+
+    @Test
+    @DisplayName("1. should fetch all reply comments for parent comment")
+    void fetchAllReplyComments() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + this.testUserToken);
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate
+                .exchange("/api/comments/1", HttpMethod.GET, request, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode(), "Should return 200 OK");
+        System.out.println("response: " + response);
+
+    }
+
+    @Test
+    @DisplayName("2. should create a comment and return it's uri.")
+    @DirtiesContext
+    void createCommentAndReturnUri() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + this.testUserToken);
+
+        String commentText = "Test Comment #2 on Test Post 1";
+
+        HttpEntity<String> request = new HttpEntity<>(commentText, headers);
+
+        ResponseEntity<String> response = restTemplate
+                .exchange("/api/posts/1/comments", HttpMethod.POST, request, String.class);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode(), "Should return 201 Created");
+
+        System.out.println("response: " + response);
+
+        URI uri = response.getHeaders().getLocation();
+        System.out.println(uri.toString());
+        request = new HttpEntity<>(headers);
+        response = restTemplate
+                .exchange(uri, HttpMethod.GET, request, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode(), "Should return 200 OK");
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        System.out.println("response: " + response);
+    }
+
+    @Test
+    @DisplayName("3. should create a reply comment and return it's uri.")
+    @DirtiesContext
+    void createReplyCommentAndReturnUri() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + this.testUserToken);
+        String commentText = "Test Reply 3 to Comment 1 on Test Post 1";
+
+        HttpEntity<String> request = new HttpEntity<>(commentText, headers);
+
+        ResponseEntity<String> response = restTemplate
+                .exchange("/api/posts/1/comments?parentId=1", HttpMethod.POST, request, String.class);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode(), "Should return 201 Created");
+
+        System.out.println("response: " + response);
+
+        URI uri = response.getHeaders().getLocation();
+        System.out.println(uri.toString());
+        request = new HttpEntity<>(headers);
+        response = restTemplate
+                .exchange(uri, HttpMethod.GET, request, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode(), "Should return 200 OK");
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        System.out.println("response: " + response);
+    }
+
+    @Test
+    @DisplayName("4. should fetch single comment by id.")
+    void fetchSingleCommentById() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + this.testUserToken);
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate
+                .exchange("/api/comments/comment/1", HttpMethod.GET, request, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode(), "Should return 200 OK");
+        System.out.println("response: " + response);
+    }
+
+}
