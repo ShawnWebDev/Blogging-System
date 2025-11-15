6e700e9cfd1a944eafceb02cfca71e09f60f3ef2
shawn <shawnby222@gmail.com>
2025-11-15 13:35:58 -0600

    Simplifies and optimizes BlogEntryResponseDto constructor

--- src/main/java/com/webdev/bloggingsystem/entities/BlogEntryResponseDto.java
+++ src/main/java/com/webdev/bloggingsystem/entities/BlogEntryResponseDto.java
@@ -1,9 +1,7 @@
 package com.webdev.bloggingsystem.entities;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public record BlogEntryResponseDto(
         Integer id,
@@ -16,15 +14,6 @@ public record BlogEntryResponseDto(
         List<CommentResponseDto> comments
 ) {
     public BlogEntryResponseDto(BlogEntry entry, List<CommentResponseDto> comments) {
-        Set<Category> categorySet = entry.getCategories();
-        // only fetch comments if specified to maintain lazy load
-        List<String> categories = new ArrayList<>(categorySet.size());
-
-        for (Category curr : categorySet) {
-            categories.add(curr.getCategoryName());
-        }
-
-        // "this" default constructor
         this(
             entry.getId(),
             entry.getAuthor().getUsername(),
@@ -32,7 +21,7 @@ public record BlogEntryResponseDto(
             entry.getContent(),
             entry.getCreatedAt(),
             entry.getUpdatedAt(),
-            categories,
+            entry.getCategories().stream().map(Category::getCategoryName).toList(),
             comments
         );
     }
