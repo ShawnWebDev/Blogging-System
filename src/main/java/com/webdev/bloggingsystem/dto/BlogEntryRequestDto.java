a71ea3be96d782cffabd9f45b1617a778aa03c45
shawn <shawnby222@gmail.com>
2025-12-30 21:06:13 -0600

    Changes minimum content size to 300 characters.

--- src/main/java/com/webdev/bloggingsystem/dto/BlogEntryRequestDto.java
+++ src/main/java/com/webdev/bloggingsystem/dto/BlogEntryRequestDto.java
@@ -11,7 +11,7 @@ public record BlogEntryRequestDto(
         @MaxBytes(255)
         @UniqueTitle
         String title,
-        @Size(min = 1000, message = "Minimum of 1000 characters")
+        @Size(min = 300, message = "Minimum of 300 characters")
         @MaxBytes(value = 65535)
         String content,
         @Size(min = 1, max = 4, message = "Must have between 1 and 4 categories")
