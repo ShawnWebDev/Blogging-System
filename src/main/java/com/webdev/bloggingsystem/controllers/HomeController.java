32eaaa5ad2771ccb2241ddf4f3639ff7afd26467
shawn <shawnby222@gmail.com>
2026-01-30 19:16:49 -0600

    Adds initial templates and mvc/htmx controller for home & auth flow

--- src/main/java/com/webdev/bloggingsystem/controllers/HomeController.java
+++ src/main/java/com/webdev/bloggingsystem/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.webdev.bloggingsystem.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,9 +13,38 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    @GetMapping("/loginSuccess")
+    public String loginSuccess(HttpServletResponse response) {
+        response.setHeader("HX-Trigger", "loginSuccess");
+        return "components/header-components::auth-head";
+    }
+
+    @GetMapping("/loginError")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "/components/header-components::login-form";
+    }
+
+    @GetMapping("/logoutSuccess")
+    public String logout(Model model, HttpServletResponse response) {
+        response.setHeader("HX-Trigger", "logoutSuccess");
+        model.addAttribute("logout", true);
+        return "/components/header-components::login-form";
+    }
+
+    @GetMapping("/commentForm")
+    public String commentForm() {
+        return "/components/comment-components::comment-form";
+    }
+
+    @GetMapping("/remove")
+    public String remove() {
+        return "";
     }
 
 }
