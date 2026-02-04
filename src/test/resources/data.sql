001c7d7671e5fc84860d697fffbe7ad6f76495f3
shawn <shawnby222@gmail.com>
2026-02-03 19:16:22 -0600

    updates schema and test data sql files to new model

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,36 +1,26 @@
-INSERT INTO Roles(role)
-VALUES
-    ('USER'),
-    ('ADMIN');
 
-INSERT INTO Users(username, password, email, date_created, is_active)
+INSERT INTO `users` (username, password, date_created, is_active, role)
 VALUES
-    ('TestAdmin', 'testPassword', 'TestEmail@email.com', '2025-10-05', TRUE),
-    ('TestUser', 'testPassword', 'TestEmail@email.com', '2025-10-05', TRUE),
-    ('TestUser2', 'testPassword', 'TestEmail@email.com', '2025-10-15', TRUE),
-    ('Te5tU$er', 'testPassword', 'TestEmail@email.com', '2026-01-03', TRUE);
+    ('TestAdmin', '$2a$10$bKQEC46DI0S.2SlYfTxLZOM9dabIOFaeQ/eOW8raycdxgopNVAc8y', '2025-10-05', TRUE, 'ADMIN'),
+    ('TestUser', '$2a$10$bKQEC46DI0S.2SlYfTxLZOM9dabIOFaeQ/eOW8raycdxgopNVAc8y', '2025-10-05', TRUE, 'USER'),
+    ('TestUser2', '$2a$10$bKQEC46DI0S.2SlYfTxLZOM9dabIOFaeQ/eOW8raycdxgopNVAc8y', '2025-10-15', TRUE, 'USER'),
+    ('Te5tU$er', '$2a$10$bKQEC46DI0S.2SlYfTxLZOM9dabIOFaeQ/eOW8raycdxgopNVAc8y', '2026-01-03', TRUE, 'USER');
 
-INSERT INTO Users_Roles(user_id, role_id)
-VALUES
-    (1, 1),
-    (1, 2),
-    (2, 1),
-    (3, 1);
 
-INSERT INTO Categories(category, description)
+INSERT INTO `categories` (category, description)
 VALUES
-    ('Test Category 1', 'Just a test category.'),
+    ('Test Category 1', 'Just a test category 1.'),
     ('Test Category 2', 'Just a test category 2.'),
     ('Test Category 3', 'Just a test category 3.');
 
-INSERT INTO Blog_Entries(title, date_published, date_updated, content, is_public, author_id)
+INSERT INTO `blog_entries` (title, date_published, date_updated, content, is_public, author_id)
 VALUES
-    ('Test Post 1', '2025-09-30', '2025-09-30','Test Post 1 - TestAdmin content is here.',TRUE, 1),
-    ('Test Post 2', '2025-10-02', '2025-10-03', 'Test Post 2 - TestAdmin content is here.',FALSE, 1),
-    ('Test Post 3', '2025-10-03', '2025-10-05','Test Post 3 - TestUser content is here.',TRUE, 2),
-    ('Test Post 4', '2025-10-03', '2025-12-31','Test Post 4 - TestUser2 content is here.',TRUE, 3);
+    ('Test Public Post 1', '2025-09-30', '2025-09-30','Test Post 1 - TestAdmin content is here.',TRUE, 1),
+    ('Test Private Post 2', '2025-10-02', '2025-10-03', 'Test Private Post 2 - TestAdmin content is here.',FALSE, 1),
+    ('Test Public Post 2', '2025-10-03', '2025-10-05','Test Public Post 2 - TestUser content is here.',TRUE, 2),
+    ('Test Public Post 3', '2025-10-03', '2025-12-31','Test Public Post 3 - TestUser2 content is here.',TRUE, 3);
 
-INSERT INTO Posts_Categories(post_id, category_id)
+INSERT INTO `posts_categories` (post_id, category_id)
 VALUES
     (1,1),
     (1,2),
@@ -41,7 +31,7 @@ VALUES
     (3,3),
     (4,3);
 
-INSERT INTO Comments(content, date_created, parent_comment_id, author_id, post_id)
+INSERT INTO `comments` (content, date_created, parent_comment_id, author_id, post_id)
 VALUES
     ('Test Comment on Test Post 1', '2025-10-01', null, 1, 1),
     ('Test Reply 1 to Comment 1 on Test Post 1', '2025-10-02', 1, 2, 1),
