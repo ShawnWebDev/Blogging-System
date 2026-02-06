af43b122b0b98f74bd7b299835ff80273dc0ef09
shawn <shawnby222@gmail.com>
2026-02-06 15:57:58 -0600

    Adds aggregate collection of category names to SimpleBlogEntry DTO

--- src/main/java/com/webdev/bloggingsystem/entities/DTO/SimpleBlogEntry.java
+++ src/main/java/com/webdev/bloggingsystem/entities/DTO/SimpleBlogEntry.java
@@ -1,6 +1,8 @@
 package com.webdev.bloggingsystem.entities.DTO;
 
 import java.time.Instant;
+import java.util.List;
 
-public record SimpleBlogEntry(int id, String title, String description, Instant createdAt) {
+public record SimpleBlogEntry(
+        int id, String title, String description, Instant createdAt, List<String> categories) {
 }
