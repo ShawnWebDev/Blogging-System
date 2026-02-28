26592b5ddaa42af0dbda6e7d93563a960ec5c46c
shawn <shawnby222@gmail.com>
2026-02-27 18:44:24 -0600

    Adds initial blogentryservicetest class

--- src/test/java/com/webdev/bloggingsystem/unit/services/BlogEntryServiceTest.java
+++ src/test/java/com/webdev/bloggingsystem/unit/services/BlogEntryServiceTest.java
@@ -3,10 +3,12 @@ package com.webdev.bloggingsystem.unit.services;
 import com.webdev.bloggingsystem.blog.BlogEntryDao;
 import com.webdev.bloggingsystem.blog.CategoryDao;
 import com.webdev.bloggingsystem.blog.BlogEntryService;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tools.jackson.databind.ObjectMapper;
 
 @ExtendWith(MockitoExtension.class)
 public class BlogEntryServiceTest {
@@ -14,9 +16,14 @@ public class BlogEntryServiceTest {
     BlogEntryDao blogEntryDao;
     @Mock
     CategoryDao categoryDao;
+    @Mock
+    ObjectMapper mapper;
 
     @InjectMocks
     BlogEntryService blogEntryService;
+
+    //todo : create unit tests for BlogEntryService methods.
+
 
 
 }
