d85f803d70bd9a959e9f39c9a6044c94e2ff69e4
shawn <shawnby222@gmail.com>
2026-03-01 00:03:26 -0600

    Adds a create static factory method

--- src/main/java/com/webdev/bloggingsystem/blog/FullBlogEntryDto.java
+++ src/main/java/com/webdev/bloggingsystem/blog/FullBlogEntryDto.java
@@ -3,11 +3,18 @@ package com.webdev.bloggingsystem.blog;
 import java.time.Instant;
 import java.util.List;
 
-public record FullBlogEntryDto(String title,
-                               String slug,
-                               String description,
-                               Instant createdAt,
-                               Instant updatedAt,
-                               List<String> categories,
-                               List<BlogEntryContentBlockDto> blocks) {
+public record FullBlogEntryDto(int id,
+                                String title,
+                                String slug,
+                                String description,
+                                Instant createdAt,
+                                Instant updatedAt,
+                                List<String> categories,
+                                List<BlogEntryContentBlockDto> blocks) {
+
+    public static FullBlogEntryDto create(BlogEntry entry, List<BlogEntryContentBlockDto> blocks) {
+        return new FullBlogEntryDto(
+                entry.getId(), entry.getTitle(), entry.getSlug(), entry.getDescription(), entry.getCreatedAt(), entry.getUpdatedAt(),
+                entry.getCategoryNames(), blocks);
+    }
 }
