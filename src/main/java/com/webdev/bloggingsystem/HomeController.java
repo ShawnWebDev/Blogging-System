6ed2c4829ed5b9c04d2f0ad99240e90ab3d89329
shawn <shawnby222@gmail.com>
2026-03-01 18:48:15 -0600

    Adds initial contact and portfolio templates

--- src/main/java/com/webdev/bloggingsystem/HomeController.java
+++ src/main/java/com/webdev/bloggingsystem/HomeController.java
@@ -1,5 +1,7 @@
 package com.webdev.bloggingsystem;
 
+import com.webdev.bloggingsystem.blog.BlogEntryDao;
+
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxRequest;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxTrigger;
@@ -7,11 +9,18 @@ import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxTrigger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.FragmentsRendering;
 
 
 @Controller
 public class HomeController {
+
+    private final BlogEntryDao blogEntryDao;
+
+    public HomeController(BlogEntryDao blogEntryDao) {
+        this.blogEntryDao = blogEntryDao;
+    }
 
     @GetMapping("/")
     public FragmentsRendering home(Model model, HtmxResponse htmxResponse, HtmxRequest htmxRequest) {
@@ -22,27 +31,65 @@ public class HomeController {
         if (htmxRequest.getCurrentUrl() == null || !htmxRequest.getCurrentUrl().endsWith("/")) {
             htmxResponse.setPushUrl("/");
         }
-        return FragmentsRendering.fragment("index::about-main").build();
+        return FragmentsRendering
+                .fragment("index::welcome-title")
+                .fragment("index::about-main")
+                .build();
+    }
+
+    @GetMapping("/portfolio")
+    public FragmentsRendering portfolio(Model model, HtmxResponse htmxResponse, HtmxRequest htmxRequest) {
+        if (!htmxRequest.isHtmxRequest()) {
+            model.addAttribute("fromPortfolio", true);
+            return FragmentsRendering.fragment("portfolio").build();
+        }
+        if (htmxRequest.getCurrentUrl() == null || !htmxRequest.getCurrentUrl().endsWith("/portfolio")) {
+            htmxResponse.setPushUrl("/portfolio");
+        }
+
+        return FragmentsRendering
+                .fragment("portfolio::portfolio-title")
+                .fragment("portfolio::portfolio-main")
+                .build();
+    }
+
+    @GetMapping("/contact")
+    public FragmentsRendering contact(Model model, HtmxResponse htmxResponse, HtmxRequest htmxRequest) {
+        if (!htmxRequest.isHtmxRequest()) {
+            model.addAttribute("fromContact", true);
+            return FragmentsRendering.fragment("contact").build();
+        }
+        if (htmxRequest.getCurrentUrl() == null || !htmxRequest.getCurrentUrl().endsWith("/contact")) {
+            htmxResponse.setPushUrl("/contact");
+        }
+
+        return FragmentsRendering.fragment("contact::contact-main").build();
     }
 
     @HxTrigger("loginSuccess")
     @GetMapping("/loginSuccess")
     public FragmentsRendering loginSuccess() {
-        return FragmentsRendering.fragment("components/auth-components::logout-form").build();
+        return FragmentsRendering
+                .fragment("components/auth-components::logout-button")
+                .build();
     }
 
     @HxTrigger("logoutSuccess")
     @GetMapping("/logoutSuccess")
-    public FragmentsRendering logout(Model model) {
-
-        model.addAttribute("logout", true);
-        return FragmentsRendering.fragment("components/auth-components::login-form").build();
+    public FragmentsRendering logout(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("logout", "You have been logged out.");
+        return FragmentsRendering
+                .fragment("components/auth-components::login-button")
+                .header("HX-Refresh", "true")
+                .build();
     }
 
     @GetMapping("/loginError")
     public FragmentsRendering loginError(Model model) {
         model.addAttribute("loginError", true);
-        return FragmentsRendering.fragment("components/auth-components::login-form").build();
+        return FragmentsRendering
+                .fragment("components/auth-components::login-dialog")
+                .build();
     }
 
 }
