936d129f9fbaff37fd032a5cdc09e27719d54643
shawn <shawnby222@gmail.com>
2026-03-06 22:46:53 -0600

    Sets session creation policy so session is loaded before render.

--- src/main/java/com/webdev/bloggingsystem/SecurityConfig.java
+++ src/main/java/com/webdev/bloggingsystem/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,6 +23,7 @@ public class SecurityConfig {
                 .csrf(Customizer.withDefaults())
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
                         .maximumSessions(1).maxSessionsPreventsLogin(false).expiredUrl("/blog?logout=true")
                 )
                 .authorizeHttpRequests(requests -> requests
