17caf75eb14d56e121c3b065420b63301d0fdf6b
shawn <shawnby222@gmail.com>
2026-03-09 12:47:02 -0500

    Separates DAOs from controller and removes duplicated logic for adding to models.

--- src/main/java/com/webdev/bloggingsystem/blog/BlogController.java
+++ src/main/java/com/webdev/bloggingsystem/blog/BlogController.java
@@ -22,38 +22,33 @@ public class BlogController {
     private static final Logger logger = LoggerFactory.getLogger(BlogController.class);
     private static final int PAGE_SIZE = 10;
 
-    private final BlogEntryDao blogEntryDao;
-    private final CategoryDao categoryDao;
     private final BlogEntryService blogEntryService;
 
-    public BlogController(BlogEntryDao blogEntryDao, CategoryDao categoryDao, BlogEntryService blogEntryService) {
-        this.blogEntryDao = blogEntryDao;
-        this.categoryDao = categoryDao;
+    public BlogController(BlogEntryService blogEntryService) {
         this.blogEntryService = blogEntryService;
     }
 
-    private static void populateCreatePostModel(Model model, CreateBlogEntryDto post, List<SimpleCategoryDto> categoryList, boolean isEditing) {
-        model.addAttribute("categoryList", categoryList); // SimpleCategoryDto
+    private void populateCreatePostModel(Model model, CreateBlogEntryDto post, boolean isEditing) {
+        model.addAttribute("categoryList", blogEntryService.findAllSimpleCategories());
         model.addAttribute("blockTypes", BlockType.values());
         model.addAttribute("post", post);
         model.addAttribute("isEditing", isEditing);
+        model.addAttribute("title", isEditing ? "Edit Post | Shawn Osborne" : "Create Post | Shawn Osborne");
     }
 
-    private static void populateBlogDashboardModel(Model model, List<SimpleBlogEntryDto> posts, List<Category> categories, String categoryName, String categoryDescription) {
+    private void populateBlogDashboardModel(Model model, String categoryName, String categoryDescription) {
         model.addAttribute("title", "Blog | Shawn Osborne");
-        model.addAttribute("posts", posts);
         model.addAttribute("categoryName", categoryName);    // used for dynamic heading of 'all-posts' fragment with category filter
-        if (categories != null) model.addAttribute("categories", categories); // Full Category with description
         model.addAttribute("categoryDesc", categoryDescription);
     }
 
-    private static void populateSinglePostModel(Model model, FullBlogEntryDto entryDto) {
+    private void populateSinglePostModel(Model model, FullBlogEntryDto entryDto) {
         model.addAttribute("entry", entryDto);
         model.addAttribute("fromBlog", true);
         model.addAttribute("title", entryDto.slug());
     }
 
-    // called when requesting main blog template
+    // called when requesting main blog dashboard
     @GetMapping
     public FragmentsRendering blog(Model model, HtmxResponse htmxResponse, HtmxRequest htmxRequest,
                         @RequestParam(value = "pageNumber", defaultValue = "1", required = false) int pageNumber,
@@ -61,9 +56,9 @@ public class BlogController {
 
         // will need count to show how many pages are available when I implement that part.
         // int count = blogEntryDao.count();
-        List<SimpleBlogEntryDto> posts = blogEntryDao.findAllSimple(pageNumber, PAGE_SIZE);
-        List<Category> categories = categoryDao.findAll();
-        populateBlogDashboardModel(model, posts, categories, "All", "Select a category to filter.");
+        populateBlogDashboardModel(model, "All", "Select a category to filter.");
+        model.addAttribute("posts", blogEntryService.findAllSimpleBlogEntries(pageNumber, PAGE_SIZE));
+        model.addAttribute("categories", blogEntryService.findAllCategories());
 
         if (logout != null) {
             model.addAttribute("logout", "You have logged out.");
@@ -85,6 +80,32 @@ public class BlogController {
         return FragmentsRendering
                 .fragment("components/shared-head::head-title")
                 .fragment("blog::blog-main")
+                .build();
+    }
+
+    // called when filtering posts by category in main blog dashboard
+    @HxRequest
+    @GetMapping("/blogComponent/posts")
+    public FragmentsRendering posts(Model model,
+                                    @RequestParam(value = "categoryName", defaultValue = "All", required = false) String categoryName,
+                                    @RequestParam(value = "pageNumber", defaultValue = "1", required = false) Integer pageNumber) {
+
+        List<SimpleBlogEntryDto> filteredBlogEntries;
+        String categoryDescription;
+
+        if (categoryName.equals("All")) {
+            filteredBlogEntries = blogEntryService.findAllSimpleBlogEntries(pageNumber, PAGE_SIZE);
+            categoryDescription = "Select a category to filter.";
+        } else {
+            filteredBlogEntries = blogEntryService.findAllSimpleBlogEntriesFiltered(categoryName, pageNumber, PAGE_SIZE);
+            categoryDescription = blogEntryService.findCategoryDescriptionByName(categoryName);
+        }
+
+        populateBlogDashboardModel(model, categoryName, categoryDescription);
+        model.addAttribute("posts", filteredBlogEntries);
+
+        return FragmentsRendering
+                .fragment("components/blog-components::all-posts")
                 .build();
     }
 
@@ -116,9 +137,7 @@ public class BlogController {
     // called when requesting blog entry input form template
     @GetMapping("/post/createPost")
     public FragmentsRendering createPostView(Model model, HtmxResponse htmxResponse, HtmxRequest htmxRequest) {
-        List<SimpleCategoryDto> categoryDtos = categoryDao.findAllNames();
-        boolean isEditing = false;
-        populateCreatePostModel(model, new CreateBlogEntryDto(), categoryDtos, isEditing);
+        populateCreatePostModel(model, new CreateBlogEntryDto(), false);
 
         if (!htmxRequest.isHtmxRequest()) {
             // for refresh or direct to /blog/createPost
@@ -141,9 +160,7 @@ public class BlogController {
                               BindingResult result, Model model) {
 
         if (result.hasErrors()) {
-            List<SimpleCategoryDto> categoryDtos = categoryDao.findAllNames();
-            boolean isEditing = false;
-            populateCreatePostModel(model, createBlogEntryDto, categoryDtos, isEditing);
+            populateCreatePostModel(model, createBlogEntryDto, false);
 
             return FragmentsRendering
                     .fragment("create-post::create-post")
@@ -162,11 +179,7 @@ public class BlogController {
     public FragmentsRendering editPost(Model model, @PathVariable Integer id,
                                        HtmxRequest htmxRequest) {
         CreateBlogEntryDto dto = blogEntryService.buildCreateDto(id);
-        List<SimpleCategoryDto> categoryDtos = categoryDao.findAllNames();
-        boolean isEditing = true;
-        populateCreatePostModel(model, dto, categoryDtos, isEditing);
-
-        model.addAttribute("isEditing", true);
+        populateCreatePostModel(model, dto, true);
 
         if (!htmxRequest.isHtmxRequest()) {
             return FragmentsRendering
@@ -184,9 +197,7 @@ public class BlogController {
     public Object editPost(@Valid @ModelAttribute("post") CreateBlogEntryDto createBlogEntryDto,
                                        BindingResult result, Model model) {
         if (result.hasErrors()) {
-            List<SimpleCategoryDto> categoryDtos = categoryDao.findAllNames();
-            boolean isEditing = true;
-            populateCreatePostModel(model, createBlogEntryDto, categoryDtos, isEditing);
+            this.populateCreatePostModel(model, createBlogEntryDto, true);
 
             return FragmentsRendering
                     .fragment("create-post::create-post")
@@ -199,7 +210,7 @@ public class BlogController {
                 .build();
     }
 
-    // todo : finish this endpoint and delete service method.
+    // todo : finish this endpoint and finish delete service method.
     @HxRequest
     @DeleteMapping("/post/deletePost/{id}")
     public ResponseEntity<Void> deletePost(@PathVariable Integer id) {
@@ -207,31 +218,6 @@ public class BlogController {
 
         return ResponseEntity.ok()
                 .header("HX-Redirect", "/blog")
-                .build();
-    }
-
-    // called when filtering posts by category
-    @HxRequest
-    @GetMapping("/blogComponent/posts")
-    public FragmentsRendering posts(Model model,
-                        @RequestParam(value = "categoryName", defaultValue = "All", required = false) String categoryName,
-                        @RequestParam(value = "pageNumber", defaultValue = "1", required = false) Integer pageNumber) {
-
-        List<SimpleBlogEntryDto> filteredBlogEntries;
-        String categoryDescription;
-
-        if (categoryName.equals("All")) {
-            filteredBlogEntries = blogEntryDao.findAllSimple(pageNumber, PAGE_SIZE);
-            categoryDescription = "Select a category to filter.";
-        } else {
-            filteredBlogEntries = blogEntryDao.findAllSimpleBlogEntriesToCategoryName(categoryName, pageNumber, PAGE_SIZE);
-            categoryDescription = categoryDao.findCategoryDescriptionByName(categoryName);
-        }
-
-        populateBlogDashboardModel(model, filteredBlogEntries, null, categoryName, categoryDescription);
-
-        return FragmentsRendering
-                .fragment("components/blog-components::all-posts")
                 .build();
     }
 
