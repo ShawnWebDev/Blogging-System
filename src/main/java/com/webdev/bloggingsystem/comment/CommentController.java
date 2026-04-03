a9e299dbc6fed1083893f879c522813dd5be9437
shawn <shawnby222@gmail.com>
2026-04-03 16:05:44 -0500

    Inits comment section and comment controller for single-posts.

--- src/main/java/com/webdev/bloggingsystem/comment/CommentController.java
+++ src/main/java/com/webdev/bloggingsystem/comment/CommentController.java
@@ -1,29 +1,31 @@
 package com.webdev.bloggingsystem.comment;
 
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.view.FragmentsRendering;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/comment")
 public class CommentController {
 
+    @Autowired
+    private CommentDao commentDao;
 
-
-    // ** move to comment controller **
-    @HxRequest
-    @GetMapping("/blogComponent/commentForm")
-    public FragmentsRendering commentForm() {
-        return FragmentsRendering
-                .fragment("components/comment-components::comment-form-enabled")
-                .build();
-    }
 
     @HxRequest
-    @GetMapping("/blogComponent/removeCommentForm")
-    public FragmentsRendering removeCommentForm() {
-        return FragmentsRendering
-                .fragment("components/comment-components::comment-form-disabled")
-                .build();
+    @GetMapping("/commentComponent/commentForm")
+    public String commentForm() {
+        return "components/comment-components::comment-form-enabled";
     }
+
+    @GetMapping("/all/{entryId}")
+    public String allParentComments(Model model, @PathVariable Integer entryId) {
+        model.addAttribute("comments", commentDao.getParentCommentsByPostId(entryId));
+        return "components/comment-components::comment-container";
+    }
+
 }
