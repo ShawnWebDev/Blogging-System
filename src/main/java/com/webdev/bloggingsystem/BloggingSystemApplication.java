dd161942631bfed0c79d4cf1558bde8fc92da440
shawn <shawnby222@gmail.com>
2026-04-29 16:47:47 -0500

    Adds EnableCaching annotation to Application Entry point.

--- src/main/java/com/webdev/bloggingsystem/BloggingSystemApplication.java
+++ src/main/java/com/webdev/bloggingsystem/BloggingSystemApplication.java
@@ -3,9 +3,11 @@ package com.webdev.bloggingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
+@EnableCaching
 public class BloggingSystemApplication {
 
     static void main(String[] args) {
