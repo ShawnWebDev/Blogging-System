35f486fc40bd445c5daab047384a9320c35bb80b
shawn <shawnby222@gmail.com>
2026-05-19 13:39:19 -0500

    Moves all update logic inside validation if block for deleteComment().

--- src/main/java/com/webdev/bloggingsystem/comment/CommentService.java
+++ src/main/java/com/webdev/bloggingsystem/comment/CommentService.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CommentService {
@@ -78,22 +79,22 @@ public class CommentService {
     Comment deleteComment(Integer commentId) {
         // soft delete setting "deleted by Username or ADMIN"
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) throw new BlogEntryException("Please log in.");
         //these fields in auth cannot be null at this point. Security will filter non-authenticated user out.
         String username = auth.getName();
         boolean isAdmin = auth.getAuthorities().stream()
-                .anyMatch(a -> a.getAuthority().equals("ADMIN"));
+                .anyMatch(a -> Objects.equals(a.getAuthority(), "ADMIN"));
 
         Comment commentToEdit = commentDao.getCommentById(commentId)
                 .orElseThrow(() -> new BlogEntryException("Comment not found with id: " + commentId));
 
+        int isUpdated = 0;
         if (!commentToEdit.getContent().equals("Comment deleted.") &&
-                (this.validateAuthor(username, commentToEdit.getAuthor().id()) || isAdmin)) {
+                (isAdmin || this.validateAuthor(username, commentToEdit.getAuthor().id()))) {
             commentToEdit.setContent("Comment deleted.");
+            commentToEdit.setDeleted(true);
+            isUpdated = commentDao.update(commentToEdit.getId(), commentToEdit.getContent(), commentToEdit.isDeleted());
         }
-
-        commentToEdit.setDeleted(true);
-
-        int isUpdated = commentDao.update(commentToEdit.getId(), commentToEdit.getContent(), commentToEdit.isDeleted());
 
         if (isUpdated == 1) {
             return commentDao.getCommentById(commentToEdit.getId())
