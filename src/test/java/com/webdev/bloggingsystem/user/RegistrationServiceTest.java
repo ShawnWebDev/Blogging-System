92300cfc53f3a17fa0d58a94be641847bdc89c1a
shawn <shawnby222@gmail.com>
2026-05-26 15:35:44 -0500

    Removes unused PasswordEncoder dependency.

--- src/test/java/com/webdev/bloggingsystem/user/RegistrationServiceTest.java
+++ src/test/java/com/webdev/bloggingsystem/user/RegistrationServiceTest.java
@@ -8,7 +8,6 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.Instant;
 
