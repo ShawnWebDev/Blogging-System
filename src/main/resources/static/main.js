150ec5e7fed0e180584dbdc39182df05f719c635
shawn <shawnby222@gmail.com>
2026-06-05 14:30:24 -0500

    Adds HTMX trigger listener (navigationChange) to set #blog-nav-item to .active if from single-post. So if post is navigated to from /portfolio it updates correctly.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -4,15 +4,15 @@ navItems.forEach(el => {
     el.addEventListener('click', function () {
         navItems.forEach(el_2 => el_2.classList.remove('active'));
         this.classList.add('active');
-    });
-});
+    })
+})
 
 document.addEventListener("htmx:configRequest", function(e) {
     const el = document.getElementById("csrf-token");
     if (el) {
     e.detail.headers[el.dataset.header] = el.dataset.token;
     }
-});
+})
 
 document.body.addEventListener('loginSuccess', function(e) {
     document.querySelectorAll('.no-auth-msg').forEach(el => el.remove());
@@ -33,11 +33,16 @@ document.body.addEventListener('loginSuccess', function(e) {
             htmx.process(div);
         }
     })
-});
+})
+
+document.body.addEventListener('navigationChange', () => {
+    document.getElementById("portfolio-nav-item").classList.remove("active");
+    document.getElementById("blog-nav-item").classList.add("active");
+})
 
 let refreshTimer;
 
-//close dialog after 20 mins, keeps csrf token fresh by re-fetching when modal re-opened
+//close dialog after 30 mins
 function startCloseDialogTimer(elementId) {
     refreshTimer = setTimeout(() => {
         closeDialog(elementId);
