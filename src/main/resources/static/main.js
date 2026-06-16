907da004f41602c0f296c315d678c36f2f6015c1
shawn <shawnby222@gmail.com>
2026-06-16 18:39:47 -0500

    Adds hamburger menu for smaller screens.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,18 +1,21 @@
-
 const navItems = document.querySelectorAll('.nav-item');
+const navContainer = document.getElementById('nav-container');
+
 navItems.forEach(el => {
     el.addEventListener('click', function () {
         navItems.forEach(el_2 => el_2.classList.remove('active'));
         this.classList.add('active');
+        navContainer.classList.remove('nav-container-open');
+        navMenuToggle.innerText = '☰';
     })
-})
+});
 
 document.addEventListener("htmx:configRequest", function(e) {
     const el = document.getElementById("csrf-token");
     if (el) {
     e.detail.headers[el.dataset.header] = el.dataset.token;
     }
-})
+});
 
 document.body.addEventListener('loginSuccess', function(e) {
     document.querySelectorAll('.no-auth-msg').forEach(el => el.remove());
@@ -33,12 +36,12 @@ document.body.addEventListener('loginSuccess', function(e) {
             htmx.process(div);
         }
     })
-})
+});
 
 document.body.addEventListener('navigationChange', () => {
     document.getElementById("portfolio-nav-item").classList.remove("active");
     document.getElementById("blog-nav-item").classList.add("active");
-})
+});
 
 let refreshTimer;
 
@@ -68,12 +71,30 @@ function fetchNewCsrfToken() {
     htmx.ajax('GET', '/refresh-token', {swap: 'none'});
 }
 
-// TODO -->
+const navMenuToggle = document.getElementById('menu-toggle-btn');
+navMenuToggle.addEventListener('click', openNavMenu);
+
+function openNavMenu() {
+    navContainer.classList.toggle('nav-container-open');
+    if (navContainer.classList.contains('nav-container-open')) {
+        navMenuToggle.innerText = 'X';
+        navMenuToggle.classList.add('menu-open-toggle-btn');
+    } else {
+        navMenuToggle.innerText = '☰';
+        navMenuToggle.classList.remove('menu-open-toggle-btn');
+    }
+}
+
+// TODO :
+//  called from auth-components.login-dialog fragment
 function convertTimeToLocal() {
     console.log('login message fragment');
     const timeEl = document.getElementById('timeExpiration');
     if (!timeEl) {
         return;
     }
-    console.log("time element exists.. convert: " + timeEl.innerText);
+    const time = timeEl.textContent;
+    const date = new Date(time);
+    console.log("time element exists.. convert: " + time);
+    console.log("converted to: " + date);
 }
